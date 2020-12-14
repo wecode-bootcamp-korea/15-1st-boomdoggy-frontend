638278e57084021c584bffc118fd351e92866546
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2020-12-14 16:43:17 +0900

    initialize settings

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
+import ProductList from './Pages/ProductList/ProductList';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/product-list" component={ProductList} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
