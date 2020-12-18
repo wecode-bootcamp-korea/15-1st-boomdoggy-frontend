6219a51f25f4bc2406bd779928789645a2cd7bad
김영광 <kim-young-gwang@gim-yeong-gwang-ui-MacBookPro.local>
2020-12-18 17:06:45 +0900

    search컴포넌트 생성

---
+++ src/Components/Search/SearchBarModal.jsx
@@ -0,0 +1,47 @@
+import './SearchBarModal.scss';
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  modalToggle = () => {
+    this.setState(prevState => ({ isModalOpen: !prevState.isModalOpen }));
+  };
+
+  render() {
+    return (
+      <div
+        className={!this.state.isModalOpen ? 'SearchBar' : `SearchBar active`}
+      >
+        <div className="SearchContainer">
+          <div className="Searchleft">
+            <form className="SearchForm">
+              <input
+                type="search"
+                placeholder="Search"
+                className="searchInput"
+              />
+              <button className="searchBtn">
+                <img
+                  src="../images/search.png"
+                  alt="search"
+                  className="searchImg"
+                />
+              </button>
+            </form>
+          </div>
+          <div className="SearchRight">
+            <button className="closeBtn">
+              <img src="../images/close.png" alt="close" className="closeImg" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
