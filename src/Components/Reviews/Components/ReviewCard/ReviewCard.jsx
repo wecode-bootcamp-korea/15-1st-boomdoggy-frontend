6c4e450244229faa8e96078bf5bdf97daefa5303
pumpkinmoonshine <hay0914@gmail.com>
2020-12-21 23:08:45 +0900

    Modify: reviewCard key값 백엔드와 동일하게 수정

--- src/Components/Reviews/Components/ReviewCard/ReviewCard.jsx
+++ src/Components/Reviews/Components/ReviewCard/ReviewCard.jsx
@@ -3,24 +3,24 @@ import './ReviewCard.scss';
 
 class ReviewCard extends Component {
   render() {
-    const { review } = this.props;
+    const { review_list } = this.props;
     return (
       <section className="reviewCard">
         <div className="review imgBox">
-          <img alt="review img" src={review.img} />
+          <img alt="review img" src={review_list.image} />
         </div>
         <div className="review contents">
           <div className="starsRate">
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
           </div>
-          <p className="contentsBody">{review.text}</p>
+          <p className="contentsBody">{review_list.content}</p>
           <div className="user">
-            <span className="userName">{review.username}</span>
-            <span className="date">{review.date}</span>
+            <span className="userName">{review_list.name}</span>
+            <span className="date">{review_list.date}</span>
           </div>
         </div>
       </section>
--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './Reviews.scss';
 import ReviewCard from './Components/ReviewCard/ReviewCard';
+import './Reviews.scss';
 
 class Reviews extends Component {
   constructor() {
@@ -9,8 +9,9 @@ class Reviews extends Component {
       reviewList: {},
     };
   }
+
   componentDidMount() {
-    fetch(`http://localhost:3000/data/review.json`)
+    fetch(`http://localhost:3000/data/review_list.json`)
       .then(res => res.json())
       .then(res => {
         this.setState({ reviewList: res });
@@ -18,15 +19,17 @@ class Reviews extends Component {
   }
 
   render() {
-    const { review } = this.state.reviewList;
-    console.log(this.state.reviewList.review);
+    const { review_list } = this.state.reviewList;
+
     return (
       <section className="reviewsWrapper">
         <h2 className="contentsTitle">Our Best Reviews</h2>
         <div className="reviewRow">
-          {review &&
-            review.map(review => {
-              return <ReviewCard key={review.id} review={review} />;
+          {review_list &&
+            review_list.map(review_list => {
+              return (
+                <ReviewCard key={review_list.id} review_list={review_list} />
+              );
             })}
         </div>
       </section>
