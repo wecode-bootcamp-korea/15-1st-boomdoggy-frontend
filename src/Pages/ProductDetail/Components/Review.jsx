91bf50c380ebdf79d00681b39893e2579d6d9efb
김영광 <kim-young-gwang@gim-yeong-gwang-ui-MacBookPro.local>
2020-12-21 10:14:41 +0900

    중간저장

--- src/Pages/ProductDetail/Components/Review.jsx
+++ src/Pages/ProductDetail/Components/Review.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import './Review.scss';
+class Review extends Component {
+  state = {
+    reviews: [],
+  };
 
-class Reviews extends Component {
+  componentDidMount() {
+    fetch(''){
+
+    }
+  }
   render() {
     return (
       <div className="Review">
         <div className="itemContainer">
-          <img src="../images/cutedog.png" alt="" />
+          <div className="reviewImg">
+            <img src="../images/cutedog.png" alt="" />
+          </div>
           <div className="reviewDetail">
-            <div className="reviewTitle">1234</div>
+            <div className="reviewTitle">
+              <h4>1234</h4>
+            </div>
             <div className="reviewDate">2012.12.23</div>
             <div className="reviewText">
-              123123123123123123 123123123 213123 123123 123 213123123 213213
-              123 123 123
+              <p>
+                123123123123123123 123123123 213123 123123 123 213123123 213213
+                123 123 123
+              </p>
             </div>
           </div>
         </div>
@@ -20,4 +35,4 @@ class Reviews extends Component {
   }
 }
 
-export default Reviews;
+export default Review;
