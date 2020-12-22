376c6d02b8ddb5c684b8d3e3fc834af00277ed50
pumpkinmoonshine <hay0914@gmail.com>
2020-12-22 10:26:59 +0900

    Add: Cart 파일 생성

---
+++ src/Pages/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Cart.scss';
+
+class Cart extends Component {
+  render() {
+    return <div className="Cart">Login</div>;
+  }
+}
+
+export default Cart;
---
+++ src/Pages/Cart/Components/CartList/CartList.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './CartList.scss';
+
+class CartList extends Component {
+  render() {
+    return <section className="CartList"></section>;
+  }
+}
+
+export default CartList;
